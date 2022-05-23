be341d0d570322b0f1409cb2c484df0a00cd42b1
igorgroz <igorgroz@users.noreply.github.com>
2022-05-24 00:08:06 +1000

    travis yml feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there IG from frontend2!</p>
+        <p>Updated by the feature!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
